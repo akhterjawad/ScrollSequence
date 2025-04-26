cf59f3bebc004a6b7408b8b6fd7bf8f430bf734d
akhterjawad <akhterjawad566@gmail.com>
2025-04-26 18:56:09 +0500

    final complete

--- src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import ScrollAnimation from '../components/ScrollAnimation.jsx';
-
-export default function Home() {
-  return (
-    <div className="bg-zinc-900 m-0 p-0 overflow-hidden">
-      <ScrollAnimation />
-    </div>
-  );
-}
